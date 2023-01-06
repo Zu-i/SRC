faba06de996994f2f2fec4159f12f74bcd74f7b4
Zuri <99133001+Zuriix@users.noreply.github.com>
2023-01-06 13:06:32 +1300

    2.0.2

--- QuickReport.plugin.js
+++ QuickReport.plugin.js
@@ -2,7 +2,7 @@
  * @name QuickReport
  * @author Zu / Xion
  * @authorId 884217130923487232
- * @version 2.0.1
+ * @version 2.0.2
  * @description Helps (mainly bun) stab scammers
  * @invite recover
  * @source https://github.com/Zuriix/SRC/blob/main/QuickReport.plugin.js
@@ -55,37 +55,42 @@ module.exports = (_ => {
 			onStart () {}
 			onStop () {}
 			onMessageContextMenu (e) {
-				console.log(e);
 				if(e.instance.props.channel){
 					let channel = e.instance.props.channel;
-					let guild = BDFDB.LibraryStores.GuildStore.getGuild(channel.guild_id);
-					let vanity = guild.vanityURLCode;
-					let owner = BDFDB.LibraryStores.UserStore.getUser(guild.ownerId)
-					if (e.instance.props.message) {
-						let message = e.instance.props.message;
-						let message_url = `https://discord.com/channels/${guild.id}/${channel.id}/${message.id}`
-						let component = message.components[0] ? message.components[0].components[0] : null;
-						let author = message.author;
-						if(message.embeds.length === 1){
-							let embed = message.embeds[0];
-							console.log({
-								channel: channel,
-								guild: guild,
-								guild_owner: owner,
-								vanity: vanity,
-								message: message,
-								message_url: message_url,
-								message_author: author,
-								embed: embed,
-								verify_component: component
-							});
-							let [children, index] = BDFDB.ContextMenuUtils.findItem(e.returnvalue, {id: "devmode-copy-id"});
-							children.splice(index > -1 ? index + 1 : children.length, 0, BDFDB.ContextMenuUtils.createItem(BDFDB.LibraryComponents.MenuItems.MenuItem, {
-								label: "Create Report",
+					if(channel.guild_id){
+						let guild = BDFDB.LibraryStores.GuildStore.getGuild(channel.guild_id);
+						let vanity = guild.vanityURLCode;
+						let owner = BDFDB.LibraryStores.UserStore.getUser(guild.ownerId)
+						if (e.instance.props.message) {
+							let message = e.instance.props.message;
+							let message_url = `https://discord.com/channels/${guild.id}/${channel.id}/${message.id}`
+							let component = message.components[0] ? message.components[0].components[0] : null;
+							let author = message.author;
+							let toInject = BDFDB.ContextMenuUtils.createItem(BDFDB.LibraryComponents.MenuItems.MenuItem, {
+								label: "No Embed Found!",
 								id: BDFDB.ContextMenuUtils.createItemId(this.name, "report"),
-								icon: _ => BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.MenuItems.MenuIcon, { icon: BDFDB.LibraryComponents.SvgIcon.Names.DOWNLOAD }),
-								action: _ => {
-let output = `**WARNING:** This is a scam server.
+								icon: _ => BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.MenuItems.MenuIcon, { icon: BDFDB.LibraryComponents.SvgIcon.Names.WARNING })
+							});
+							if(message.embeds.length === 1){
+								let embed = message.embeds[0];
+								console.log({
+									channel: channel,
+									guild: guild,
+									guild_owner: owner,
+									vanity: vanity,
+									message: message,
+									message_url: message_url,
+									message_author: author,
+									embed: embed,
+									verify_component: component
+								});
+								toInject = BDFDB.ContextMenuUtils.createItem(BDFDB.LibraryComponents.MenuItems.MenuItem, {
+									label: "Create Report",
+									id: BDFDB.ContextMenuUtils.createItemId(this.name, "report"),
+									icon: _ => BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.MenuItems.MenuIcon, { icon: BDFDB.LibraryComponents.SvgIcon.Names.DOWNLOAD }),
+									action: _ => {
+										BDFDB.LibraryModules.WindowUtils.copy("Failed to copy, check devtools for errors!");
+										let output = `**WARNING:** This is a scam server.
 Note: **Please do not** harass users with hoisted roles, they're usually given out to pin blame on unrelated members.
 \`\`\`
 Link: ${vanity?"\nVanity: .gg/"+vanity:""}
@@ -96,11 +101,20 @@ Owner: ${owner.username}#${owner.discriminator} (ID: ${owner.id})
 Fake Verify Bot: ${author.username}#${author.discriminator} (ID: ${author.id})
 Fake Verify Message: ${message_url}
 \`\`\``
-									console.log(output);
-									BDFDB.LibraryModules.WindowUtils.copy(output);
-								}
-							}));
+										console.log(output);
+										BDFDB.LibraryModules.WindowUtils.copy(output);
+
+										if(BDFDB.LibraryModules.WindowUtils.readClipboard() === output){
+											BDFDB.NotificationUtils.toast("Copied to clipboard!");
+										} else {
+											BDFDB.NotificationUtils.toast("Failed to copy to clipboard!?");
+										}
+									}
+							});
 						}
+						let [children, index] = BDFDB.ContextMenuUtils.findItem(e.returnvalue, {id: "devmode-copy-id"});
+						children.splice(index > -1 ? index + 1 : children.length, 0, toInject);
+					}
 					}
 				}
 			}
